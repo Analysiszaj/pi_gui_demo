186a144c9ebefd6c8157b80827d240257723f14d
zaj <1418623546@qq.com>
2025-03-23 14:54:57 +0800

    feat

--- imgui_version/gui.cpp
+++ imgui_version/gui.cpp
@@ -1,12 +1,17 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "gui.h"
 
+void LBGui::render() {
+}
+
 void Gui::render() {
     ImGui::PushFont(default_font->font);
 
     render_header();
-    render_content();
-    render_footer();
+    // render_content();
+    // render_footer();
+
+    main_gui.render();
 
     ImGui::PopFont();
 }
@@ -32,7 +37,7 @@ void Gui::render_content() {
     ImGui::BeginChild("##CONTENT", ImVec2(0.0f, ImGui::GetContentRegionAvail().y * 0.6f));
 
     ImGui::BeginChild("##CONTENT_LEFT", ImVec2(ImGui::GetContentRegionAvail().x * 0.3f, 0.0f));
-    ImDrawList *left_draw_list = ImGui::GetBackgroundDrawList();
+    ImDrawList *left_draw_list = ImGui::GetWindowDrawList();
     draw_content_left_bg(left_draw_list);
     ImGui::EndChild();
 
@@ -104,40 +109,85 @@ void Gui::draw_header_text(const std::string text, ImVec4 text_color, ImVec4 bg_
 
 void Gui::draw_content_left_bg(ImDrawList *draw_list) {
     ImVec2 p1 = ImGui::GetWindowPos();
-    ImVec2 p2 = p1 + ImGui::GetWindowSize();
+    ImVec2 p2 = p1 + ImVec2(ImGui::GetWindowWidth(), ImGui::GetWindowHeight() * 0.18f);
+
     float width = ImGui::GetWindowWidth();
-    float height = ImGui::GetWindowHeight();
+    float height = ImGui::GetWindowHeight() * 0.18f;
 
     ImGui::PushClipRect(p1, p2, true);
-    draw_list->AddRectFilled(p1, p2, ImGui::GetColorU32(ImColor(198, 241, 248).Value), HelloImGui::EmSize(2.0f));
+    float r = 25;
+    float k = 0.3f;
+    ImVec2 p3 = p1 + ImVec2(width - r, 0.0f);
+    ImVec2 p4 = p1 + ImVec2(width - r * k, 0.0f);
+    ImVec2 p5 = p1 + ImVec2(width, r * k);
+    ImVec2 p6 = p1 + ImVec2(width, r);
+    draw_list->PathLineTo(p3);
+    draw_list->PathBezierCubicCurveTo(p4, p5, p6);
 
-    ImVec2 p3 = p1 + ImVec2(width * 0.8f, height);
-    draw_list->AddRectFilled(p1, p3, IM_COL32_BLACK);
+    draw_list->PathLineTo(p2);
+    ImVec2 p7 = p2 - ImVec2(16.0f, 0.0f);
+    draw_list->PathLineTo(p7);
+    ImVec2 p8 = p2 - ImVec2(16.0f, 8.0f);
+    draw_list->PathLineTo(p8);
 
-    ImVec2 p4 = p1 + ImVec2(width * 0.79f, height);
-    draw_list->AddRectFilled(p1, p4, ImGui::GetColorU32(ImColor(216, 246, 241).Value));
+    float r1 = 10;
+    float k1 = 0.3f;
+    ImVec2 p9 = p8 + ImVec2(0.0f, -r1 * k1 * 2.0f);
+    ImVec2 p10 = p8 + ImVec2(-r1 * k1, -r1);
+    ImVec2 p11 = p8 + ImVec2(-r1, -r1);
+    draw_list->PathBezierCubicCurveTo(p9, p10, p11, 24);
 
-    ImVec2 p5 = p1 + ImVec2(width * 0.15f, height);
-    draw_list->AddRectFilled(p1, p5, IM_COL32_BLACK);
+    ImVec2 p12 = p1 + ImVec2(width * 0.8f, height - 8.0f - 10.0f);
+    draw_list->PathLineTo(p12);
+    ImVec2 p13 = p1 + ImVec2(width * 0.8f, 0.0f);
+    draw_list->PathLineTo(p13);
+    ImVec2 p14 = p1 + ImVec2(width - r, 0.0f);
+    draw_list->PathLineTo(p14);
 
-    ImVec2 p6 = p1 + ImVec2(width * 0.13f, height);
-    draw_list->AddRectFilled(p1, p6, ImGui::GetColorU32(ImColor(215, 182, 182).Value));
+    draw_list->PathFillConcave(ImGui::GetColorU32(ImColor(198, 241, 248).Value));
 
-    ImVec2 p7 = p1 + ImVec2(width * 0.11f, height);
-    draw_list->AddRectFilled(p1, p7, IM_COL32_BLACK);
+    float new_height = height - 18.0f;
+    ImVec2 p15 = p1 + ImVec2(width * 0.80f, new_height);
+    draw_list->AddRectFilled(p1, p15, IM_COL32_BLACK, 0.0f);
 
-    ImVec2 p8 = p1 + ImVec2(width * 0.10f, height);
-    draw_list->AddRectFilled(p1, p8, ImGui::GetColorU32(ImColor(215, 182, 182).Value));
+    ImVec2 p16 = p1 + ImVec2(width * 0.79f, new_height);
+    draw_list->AddRectFilled(p1, p16, ImGui::GetColorU32(ImColor(216, 246, 241).Value));
 
-    ImVec2 p9 = p1 + ImVec2(width * 0.09f, height);
-    draw_list->AddRectFilled(p1, p9, IM_COL32_BLACK);
+    ImVec2 p17 = p1 + ImVec2(width * 0.15f, new_height);
+    draw_list->AddRectFilled(p1, p17, IM_COL32_BLACK);
 
-    ImVec2 p10 = p1 + ImVec2(width * 0.08f, height);
-    draw_list->AddRectFilled(p1, p10, ImGui::GetColorU32(ImColor(232, 245, 137).Value));
+    ImVec2 p18 = p1 + ImVec2(width * 0.13f, new_height);
+    draw_list->AddRectFilled(p1, p18, ImGui::GetColorU32(ImColor(215, 182, 182).Value));
 
-    draw_list->AddRectFilled(p1, p2, ImGui::GetColorU32(ImVec4(0.0, 0.0f, 0.0f, 0.0f)));
+    ImVec2 p19 = p1 + ImVec2(width * 0.11f, new_height);
+    draw_list->AddRectFilled(p1, p19, IM_COL32_BLACK);
+
+    ImVec2 p20 = p1 + ImVec2(width * 0.10f, new_height);
+    draw_list->AddRectFilled(p1, p20, ImGui::GetColorU32(ImColor(215, 182, 182).Value));
+
+    ImVec2 p21 = p1 + ImVec2(width * 0.09f, new_height);
+    draw_list->AddRectFilled(p1, p21, IM_COL32_BLACK);
+
+    ImVec2 p22 = p1 + ImVec2(width * 0.08f, new_height);
+    draw_list->AddRectFilled(p1, p22, ImGui::GetColorU32(ImColor(232, 245, 137).Value));
 
     ImGui::PopClipRect();
 }
+
 void Gui::draw_content_right_bg(ImDrawList *draw_list) {
+    ImVec2 p1 = ImGui::GetWindowPos();
+    ImVec2 p2 = p1 + ImGui::GetWindowSize();
+    float width = ImGui::GetWindowWidth();
+    float height = ImGui::GetWindowHeight();
+}
+
+void MainGui::render() {
+    ImGui::BeginChild("##MAIN_CONTENT", ImGui::GetContentRegionAvail());
+    ImDrawList *draw_list = ImGui::GetWindowDrawList();
+
+    ImVec2 p1 = ImGui::GetWindowPos();
+    ImVec2 p2 = p1 + ImGui::GetWindowSize();
+
+    draw_list->AddRectFilled(p1, p2, ImGui::GetColorU32(ImColor(236, 177, 156).Value), HelloImGui::EmSize(2.0f), ImDrawFlags_RoundCornersTopLeft);
+    ImGui::EndChild();
 }
--- imgui_version/gui.h
+++ imgui_version/gui.h
@@ -1,10 +1,33 @@
 #pragma once
 #include "hello_imgui/hello_imgui.h"
 #include "imgui.h"
+#include <deque>
+
+// 右下角GUI
+class LBGui {
+public:
+    LBGui() = default;
+    ~LBGui() = default;
+
+private:
+    void render();
+};
+
+// 主界面GUI
+class MainGui {
+public:
+    MainGui() = default;
+    ~MainGui() = default;
+    void render();
+
+private:
+    void render_content_bg();
+};
 
 class Gui {
 private:
     HelloImGui::FontDpiResponsive *default_font;
+    MainGui main_gui;
 
 public:
     Gui() = default;
